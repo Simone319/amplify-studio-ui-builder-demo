73444bec9d27b1e3d7feb6dcc60ae8cf99be3472
Zhang <zimzha@amazon.com>
2025-08-11 13:49:47 +0200

    fix: remove ActionCard

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -4,7 +4,6 @@
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
 
-export { default as ActionCard } from "./ActionCard";
 export { default as Ampligram } from "./Ampligram";
 export { default as CTASection } from "./CTASection";
 export { default as CheckoutPayment } from "./CheckoutPayment";
